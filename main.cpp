022ca751769b26a020cb497e5fc1ae4b5c462afd
maanasagowda <162300560+maanasagowda@users.noreply.github.com>
2024-03-09 18:59:24 +0530

    Update main.cpp
    updated the .cpp file to check build

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world!!"  "This is SRN : PES1UG21CS312"<< std::endl;
+    std::cout << "Hello, world!!"  "This is SRN : PES1UG21CS312!"<< std::endl;
     return 0;
 }
